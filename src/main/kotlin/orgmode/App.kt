6c35c0a13160beba2864faedff4f1a70108c62e6
Ilya <iliayar3@gmail.com>
2020-10-01 21:22:10 +0300

    Some test

--- src/main/kotlin/orgmode/App.kt
+++ src/main/kotlin/orgmode/App.kt
@@ -11,8 +11,8 @@ fun tic() {
     timer = System.nanoTime()
 }
 
-fun toc() {
-    println("Operation done in ${(System.nanoTime() - timer)/1000000} ms")
+fun toc(msg: String) {
+    println("$msg done in ${(System.nanoTime() - timer)/1000000} ms")
 }
 
 
@@ -39,10 +39,33 @@ fun main(args: Array<String>) {
 """)).parse()
     }
 
-    println(org.toString())
-    println(org.toJson())
+    // println(org.toString())
+    // println(org.toJson())
 
-    toc()
+    toc("Regex Parser")
+//     tic()
 
+//     if(args.size > 0) {
+// 	org = OrgParser(FileSource(args[0])).parse()
+//     } else {
+
+// 	org = OrgParser(StringSource("""
+// * Unordered List
+// - elem 1
+
+// - elem 2
+
+// * Ordered List
+
+// 1. elem 1
+// 2. elem 2
+// """)).parse()
+//     }
+
+//     // println(org.toString())
+//     // println(org.toJson())
+
+//     toc("Fucking ugly parser")
     File("/tmp/kt.html").writeText(org.toHtml())
+
 }
